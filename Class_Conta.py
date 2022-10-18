fb63291a98adfc5c6fcb9eb71c08cbd64361810e
Renan Almeida <101940943+Renan-Andrade-de-Almeida@users.noreply.github.com>
2022-10-18 13:01:24 -0300

    Update Class_Conta.py

--- Class_Conta.py
+++ Class_Conta.py
@@ -9,7 +9,7 @@ class Conta:
         self.saldo += valor
 
     def saca(self, valor):
-        if self.saldo < valor:
+        if self.saldo >= valor:
             self.saldo -= valor
             return True
         else:
@@ -27,4 +27,22 @@ class Conta:
             destino.deposita(valor)
             return True
     
-    
+
+conta1 = Conta((int(input("numero da conta: "))), input("titular da conta: "), int(input("saldo: ")), limite=5000)
+
+
+while(True):
+    checaOperacao = int(input("Qual operação deseja realizar?\n 1 - Depositar \n 2 - Sacar \n 3 - transferir \n\n"))
+    if(checaOperacao == 1):
+        conta1.deposita(int(input("Quanto deseja depositar? \n")))
+        break
+    elif(checaOperacao == 2):
+        conta1.saca(int(input("Quanto deseja sacar? \n")))
+        break
+    elif(checaOperacao == 3):
+        conta2 = Conta((int(input("numero da conta: "))), input("titular da conta: "), int(input("qual o saldo da conta: ")), limite=5000)
+        conta1.transfere_para(conta2, int(input("Quanto deseja transferir? \n")))
+        break
+
+print("O saldo do titular {} é: ".format(conta1.titular), conta1.saldo)
+print("O saldo do titular {} é: ".format(conta2.titular),conta2.saldo)
