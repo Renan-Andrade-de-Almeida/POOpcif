b221a6385d67588b8d8b7d56aa69a58887a5e940
Renan Almeida <101940943+Renan-Andrade-de-Almeida@users.noreply.github.com>
2022-10-21 14:13:07 -0300

    a

--- conta.py
+++ conta.py
@@ -6,9 +6,9 @@ class Historico:
         self.data_abertura = datetime.datetime.today()
         self.transacoes = []
 
-    def imprime(self):
+    def imprime(self, cliente):
         print("data abertura: {}".format(self.data_abertura))
-        print("transações: ")
+        print("transações de {}: ".format(cliente.nome))
         for t in self.transacoes:
             print("-", t)  
             
@@ -20,43 +20,44 @@ class Cliente:
         self.cpf = cpf
 
 class Conta:
-    def __init__(self, numero, cliente, saldo, limite=10000):
+    def __init__(self, numero, cliente, saldo, limite=1000.0):
         self.numero = numero
-        self.titular = cliente
+        self.cliente = cliente
         self.saldo = saldo
         self.limite = limite
+        self.historico = Historico()
 
     def deposita(self, valor):
         self.saldo += valor
-    
+        self.historico.transacoes.append("depósito de {}".format(valor))
+
     def saca(self, valor):
         if (self.saldo < valor):
             return False
         else:
             self.saldo -= valor
-            return True
+            self.historico.transacoes.append("saque de {}".format(valor))
 
     def extrato(self):
         print("numero: {} \nsaldo: {}".format(self.numero, self.saldo))
+        self.historico.transacoes.append("tirou extrato - saldo de {}".format(self.saldo))
 
-    def transfere(self, destino, valor):
+    def transfere_para(self, destino, valor):
         retirou = self.saca(valor)
         if (retirou == False):
             return False
         else:
             destino.deposita(valor)
+            self.historico.transacoes.append("transferencia de {} para conta {}".format(valor, destino.numero))
             return True
 
 
-c1 = Conta('123-4', 'João', 120.0, 1000.0) 
-c2 = c1 
-print(c2.saldo)
-#120.0
-c1.deposita(100.0) 
-print(c1.saldo) 
-#220.0 
-c2.deposita(30.0) 
-print(c2.saldo)
-#250.0 
-print(c1.saldo) 
-#250.0
+cliente1 = Cliente('Kenya', 'Maria', '11111111111-11')
+cliente2 = Cliente('Renan', 'Almeida', '222222222-22')
+conta1 = Conta('123', cliente1, 1000.0)
+conta2 = Conta('321', cliente2, 1000.0)
+conta1.deposita(100.0)
+conta1.saca(50.0)
+conta1.transfere_para(conta2, 200.0)
+conta1.extrato
+conta1.historico.imprime(cliente1)
